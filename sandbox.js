698fb6bdd129ad0d1f52004620c2e759f6593fec
Clifford Baidoo <baidooclifford56@gmail.com>
2024-02-21 13:44:38 +0000

    trying java script

---
+++ sandbox.js
@@ -0,0 +1,25 @@
+alert("It's me Mario");
+console.log("Come on Brother");
+
+let age = 25;
+let year = 2024;
+
+console.log(age,year);
+ age = 39;
+ console.log(age);
+
+ const points = 293;
+ console.log(points);
+
+ let email = "takemichihanma715@gmail.com";
+ console.log(email);
+
+ let firstName = "Daniel";
+ let lastName = "Greeves";
+ let fullName = firstName+' '+lastName;
+ console.log(fullName)
+
+ console.log(fullName.toUpperCase())
+
+ let index = email.indexOf('@')
+ console.log(index)
